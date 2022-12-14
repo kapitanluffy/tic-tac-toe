ebcdc7ba09469148ae0932ca2916eed8d06f4704
kapitanluffy <kapitanluffy@gmail.com>
2022-12-15 03:25:40 +0800

    Enable cors and PORT support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from "process"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  const PORT = env.PORT || 3000
+
+  await app.listen(PORT);
 }
 bootstrap();
